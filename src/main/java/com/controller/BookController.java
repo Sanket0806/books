ce1c0aef6be80991f19aa9950460e0dcde1e398b
Amolrd2002 <amoldhawale2002@gmail.com>
2024-03-30 00:11:10 +0530

    first commit

---
+++ src/main/java/com/controller/BookController.java
@@ -0,0 +1,18 @@
+package com.controller;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.entity.Book;
+import jakarta.persistence.EntityManager;
+
+@RestController
+public class BookController {
+	
+}
